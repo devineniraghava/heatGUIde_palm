8c36641a5bbadd27b3e93b3659c2d1b2a2eb2fb3
devineniraghava <64875320+devineniraghava@users.noreply.github.com>
2022-03-18 17:08:04 +0100

    new version

---
+++ static_driver/netcdf_tutorial.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 16:54:44 2022
+
+@author: rdevinen
+"""
+# https://www.youtube.com/watch?v=VH-PCQ991fw
+import datetime
+import math
+import pytz
+
+from netCDF4 import Dataset
+import numpy as np
+
+import netCDF4 as nc
+
+#%%
+
+fn = "example_static_file.nc"
+
+ds = nc.Dataset(fn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
